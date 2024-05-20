ffe0a25c8f1a61404f4e510f63c99ac4fd99e7bc
aa1as <sm13929881@gmail.com>
2024-05-20 19:37:09 +0900

    버튼클릭 이벤트

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import './Toast.css';
-import ToastMessage from './ToastMessage';
+import React from "react";
+import "./Toast.css";
+import ToastMessage from "./ToastMessage";
 
 const Toast = ({ message }) => {
+  const buttonClickHandler = (title) => {
+    console.log(title);
+  };
   return (
     <div className={`toast toast-${message.title}`}>
-      <ToastMessage message={message}/>
-      <button className="toast__button">Dismiss</button>
+      <ToastMessage message={message} />
+      <button
+        className="toast__button"
+        onClick={() => buttonClickHandler(message.title)}
+      >
+        Dismiss
+      </button>
     </div>
   );
 };
