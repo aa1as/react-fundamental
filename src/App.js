f0deed2ad78a54ede5a6d00492317804629245dc
aa1as <sm13929881@gmail.com>
2024-05-21 19:50:01 +0900

    자식 -> 부모 실습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 import PaymentForm from "./components/PaymentForm/PaymentForm";
 import Expenses from "./components/Payments/Expenses";
 
 function App() {
-	const expenses = [
-		{
-			id: "e1",
-			title: "수건",
-			amount: 12.33,
-			date: new Date(2025, 8, 14),
-		},
-	];
+  const [expenses, setExpenses] = useState([
+    {
+      id: "e1",
+      title: "수건",
+      amount: 12.33,
+      date: new Date(2025, 8, 14),
+    },
+  ]);
+  const getPaymentFormData = (data) => {
+    setExpenses([
+      {
+        id: Math.random().toString(),
+        title: data.name,
+        amount: data.price,
+        date: data.today,
+      },
+    ]);
+  };
 
-	return (
-		<>
-			<PaymentForm />
-			<Expenses items={expenses} />
-		</>
-	);
+  return (
+    <>
+      <PaymentForm getPaymentFormData={getPaymentFormData} />
+      <Expenses items={expenses} />
+    </>
+  );
 }
 
 export default App;
