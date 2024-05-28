30a2cf8230f89214df9e440fccad5823b68ff9cd
aa1as <sm13929881@gmail.com>
2024-05-28 21:28:59 +0900

    유효성 검사 실습

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const LoggedInInfo = localStorage.getItem("isLoggedIn");
+    //만약 로그인 됬다면 true
+    if (LoggedInInfo === "1") setIsLoggedIn(true);
+    //아니면 false (필수는아님 state는 렌더링할때마다 초기화 )
+    else setIsLoggedIn(false);
+  }, []);
+
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
